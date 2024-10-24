0431f61bf93c0fecc47d3c628c97c16b9a379e82
TooooooR <taras.protsiv12@gmail.com>
2024-10-24 09:16:30 +0300

    lab6
    without folder node_modules

---
+++ lab6/src/components/Catalog/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Tree from "../../images/crismas-Tree.jpg"
+import "./Catalog.css";
+
+function Card(props) {
+    return (
+        <div class="card_of_tree">
+            <img src={Tree} alt="crismasTree" class="place_for_img"></img>
+            <h4>Виробник {props.tree.manufacturer_name}</h4>
+            <p>Height: {props.tree.height_cm} cm</p>
+            <p>Price: {props.tree.price} UAH</p>
+            <p>Material: {props.tree.material}</p>
+            <div class="tree_edit_delete">
+                {/* <button class="edit_button" data-id={item.id}>Edit</button>
+                <button class="delete_button" data-id={item.id}>Delete</button> */}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ lab6/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeroSection.css";
+
+function HeroSection() {
+    return (
+        <div className="wrappers">
+            <p className="title">Штучні ялинки від виробника в Україні</p>
+        </div>
+    );
+}
+
+export default HeroSection;
